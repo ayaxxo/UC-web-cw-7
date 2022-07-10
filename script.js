3d01bc3a087a079a0aeb2b3a977fdeca05a971ab
ayaxxo <107775834+ayaxxo@users.noreply.github.com>
2022-07-10 04:01:15 +0300

    all done except bonuses

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-// عرف المصفوفة هنا
+let Courses = []
 
 function addCourse() {
   let name = document.getElementById("name").value;
   let id = document.getElementById("id").value;
   let phone = document.getElementById("phone").value;
   let course = document.getElementById("course").value;
-  
+//  let newCourse = {
+//   name:
+//   id:
+//   phone: 
+//   course:
+
+//  }
+  Courses.push({name,id,phone,course});
+  console.log(Courses);
+  load();
+  save();
 }
 
+let container = document.getElementById("container");
 function load() {
-  let container = document.getElementById("container");
-  
+  container.innerHTML="";
+  Courses.forEach((Newcourse)=>{
+    container.innerHTML+=`<div><h1>${Newcourse.name} - ${ Newcourse.id}</h1><h3>${Newcourse.course}</h3></div>`;
+    
+  });
 }
+
+function save(){
+  localStorage.setItem("Courses",JSON.stringify(Courses));
+}
+function read(){
+  Courses = JSON.parse(localStorage.getItem("Courses")||[]);
+  load();
+}
+read();
